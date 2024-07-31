a6dffd485a85d064b477e57134842b33c70857d4
Pablo Yuri <pabloyuridev@gmail.com>
2024-07-31 15:25:33 -0300

    feat: add components directory with HTML template file with Alpine.js, Tailwindcss and HTMX

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
-async fn hello() -> impl Responder {
+async fn render_example() -> impl Responder {
     HttpResponse::Ok()
         .content_type("text/html")
         .body("<h1>Go to Rust</h1>")
@@ -9,11 +9,8 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(render_example))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
