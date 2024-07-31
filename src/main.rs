1004bacff087e8f57089795a07b46021651fe140
Pablo Yuri <pabloyuridev@gmail.com>
2024-07-31 14:58:31 -0300

    feat: add actix and initial TCP configs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body("<h1>Go to Rust</h1>")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
