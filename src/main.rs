f94d40dc474fc37bc47ee356609b518c62beada3
Pablo Yuri <pabloyuridev@gmail.com>
2024-07-31 19:12:54 -0300

    feat: add htmx-request route

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,13 @@ async fn render_example() -> impl Responder {
     let mut button_data = HashMap::new();
     button_data.insert("color".to_string(), "bg-stone-100".to_string());
     button_data.insert("text".to_string(), "Increase the counter".to_string());
-    let particle_button = get_component("particles/increase_button", None, Some(&button_data));
+    let increase_particle_button =
+        get_component("particles/increase_button", None, Some(&button_data));
+    let htmx_particle_button = get_component("particles/htmx_button", None, None);
 
     let mut components = HashMap::new();
-    components.insert("button".to_string(), particle_button.unwrap());
+    components.insert("button".to_string(), increase_particle_button.unwrap());
+    components.insert("htmx_button".to_string(), htmx_particle_button.unwrap());
 
     let main_component = get_component("main", Some(&components), None);
     HttpResponse::Ok()
@@ -22,11 +25,19 @@ async fn render_example() -> impl Responder {
         .body(main_component.unwrap())
 }
 
+#[get("/htmx-request")]
+async fn htmx_example() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body("<p class=\"text-stone-100\">Request made! ;)</p>")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(render_example)
+            .service(htmx_example)
             .service(fs::Files::new("/assets", "./assets").show_files_listing())
     })
     .bind(("127.0.0.1", 8080))?
